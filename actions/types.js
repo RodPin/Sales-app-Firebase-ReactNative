757c1d81d8872dabf9cf37b565f380c7bc817906
RodPin <prodrigopcgomes@gmail.com>
2018-08-30 23:37:32 -0300

    kkkk

--- actions/types.js
+++
@@ -1,3 +0,0 @@
-export const FB_LOGIN_SUCESS = 'fb_login_sucess';
-export const FB_LOGIN_FAIL = 'fb_login_fail';
-export const FETCH_JOBS = 'fetch_jobs';
--- reducers/index.js
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from 'redux';
-import auth from './auth_reducer';
-
-//reducer tem q retornar algo
-export default combineReducers({
-    auth
-})
